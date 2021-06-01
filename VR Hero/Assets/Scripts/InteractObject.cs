af125a9ce4481810b4a4ad5a6001458aead2eb44
Wojciech Dywan <wojciech.dywan@interia.pl>
2021-06-01 12:48:39 +0200

    Fade Sign on hover interact object

---
+++ VR Hero/Assets/Scripts/InteractObject.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+public class InteractObject : MonoBehaviour
+{
+    public GameObject hoverSign;
+
+    private XRGrabInteractable xRGrabInteractable;
+
+    public bool lerpTransparent = false;
+    private List<Material> materials;
+
+    private float alpha = 0;
+    private float lerp = 0;
+    private byte a;
+
+    private bool isHover;
+    private void Start()
+    {
+        xRGrabInteractable = GetComponent<XRGrabInteractable>();
+        materials = new List<Material>();
+        foreach (Material material in hoverSign.GetComponent<Renderer>().materials)
+        {
+            materials.Add(material);
+        }
+    }
+
+    public void ShowSign()
+    {
+        // hoverSign.SetActive(true);
+        StopCoroutine("FadeOut");
+        StartCoroutine("FadeIn");
+    }
+
+    public void HideSign()
+    {
+        if (!xRGrabInteractable.isSelected)
+        {
+            StopCoroutine("FadeIn");
+            StartCoroutine("FadeOut");
+            //   hoverSign.SetActive(false);
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        lerp = 0;
+        while (lerp < 5f)
+        {
+            foreach (Material material in materials)
+            {
+                Color32 col = material.color;
+
+                alpha = Mathf.Lerp(col.a, 120f, lerp);
+
+                a = (byte)alpha;
+                lerp += Time.deltaTime * 0.05f;
+                col.a = a;
+                material.color = col;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        lerp = 0;
+        while (lerp < 5f)
+        {
+            foreach (Material material in materials)
+            {
+                Color32 col = material.color;
+
+                alpha = Mathf.Lerp(col.a, 0f, lerp);
+
+                a = (byte)alpha;
+                lerp += Time.deltaTime * 0.1f;
+                col.a = a;
+                material.color = col;
+            }
+            yield return null;
+        }
+    }
+
+
+/*    private void Update()
+    {
+        if (xRGrabInteractable.isSelected || xRGrabInteractable.isHovered)
+        {
+            //  hoverSign.SetActive(true);
+            foreach (Material material in materials)
+            {
+                Color32 col = material.color;
+
+                alpha = Mathf.Lerp(col.a, 120f, lerp);
+
+                a = (byte)alpha;
+                lerp += Time.deltaTime * 0.05f;
+                col.a = a;
+                // material.SetColor("_Color", col);
+                material.color = col;
+            }
+        }
+        else
+        {
+            //   hoverSign.SetActive(false);
+            foreach (Material material in materials)
+            {
+                Color32 col = material.color;
+
+                alpha = Mathf.Lerp(col.a, 0f, lerp);
+
+                a = (byte)alpha;
+                lerp += Time.deltaTime * 0.05f;
+                col.a = a;
+                // material.SetColor("_Color", col);
+                material.color = col;
+            }
+        }
+    }*/
+}
